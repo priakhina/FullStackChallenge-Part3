effea1168a68bb58d93b87c7348a673842ca5e13
Kseniia Priakhina <brainstorm@Kseniias-MBP.lan>
2023-07-20 17:17:39 -0600

    Add solution for exercise 3.9 (phonebook backend, step 9)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -17,26 +18,29 @@ app.use(
     )
 );
 
+// cors (https://github.com/expressjs/cors) is a middleware that enables CORS (Cross-Origin Resource Sharing) mechanism
+app.use(cors());
+
 let persons = [
     {
         id: 1,
         name: "Arto Hellas",
-        number: "040-123456",
+        phoneNumber: "040-123456",
     },
     {
         id: 2,
         name: "Ada Lovelace",
-        number: "39-44-5323523",
+        phoneNumber: "39-44-5323523",
     },
     {
         id: 3,
         name: "Dan Abramov",
-        number: "12-43-234345",
+        phoneNumber: "12-43-234345",
     },
     {
         id: 4,
         name: "Mary Poppendieck",
-        number: "39-23-6423122",
+        phoneNumber: "39-23-6423122",
     },
 ];
 
@@ -72,7 +76,7 @@ app.delete("/api/persons/:id", (req, res) => {
 app.post("/api/persons", (req, res) => {
     const body = req.body;
 
-    if (!body.name || !body.number) {
+    if (!body.name || !body.phoneNumber) {
         return res.status(400).json({
             error: "The name or number is missing",
         });
