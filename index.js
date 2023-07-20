6e429ce6bddc45953e86285fb5b84e363e76ee29
Kseniia Priakhina <brainstorm@Kseniias-MBP.lan>
2023-07-19 19:48:54 -0600

    Add solution for exercise 3.4 (phonebook backend, step 4)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-const persons = [
+let persons = [
     {
         id: 1,
         name: "Arto Hellas",
@@ -44,6 +44,13 @@ app.get("/api/persons/:id", (req, res) => {
         res.statusMessage = `No contact found with the specified id (${id})`;
         res.status(404).end();
     }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter((person) => person.id !== id);
+
+    res.status(204).end();
 });
 
 app.get("/info", (req, res) => {
